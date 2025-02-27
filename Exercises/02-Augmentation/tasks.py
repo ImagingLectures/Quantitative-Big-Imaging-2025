8ada36a188edcb7a1be37654fa15a0bfec84c0a2
Daniel Vera Nieto <daniel.vera-nieto@psi.ch>
2025-02-27 10:21:35 +0100

    Update.

--- Exercises/02-Augmentation/tasks.py
+++ Exercises/02-Augmentation/tasks.py
@@ -15,12 +15,8 @@ def create_dataset_subset(data: np.ndarray, labels: np.ndarray, n: int) -> tuple
     Returns:
         tuple[np.ndarray, np.ndarray]: 
     """
-    # raise NotImplementedError("Need to implement for task 2.1")
-    indices = np.random.choice(len(data), n, replace=False)  # Randomly select n images
-    selected_images = data[indices]
-    selected_labels = labels[indices]
-    return selected_images, selected_labels
-
+    raise NotImplementedError("Need to implement for task 2.1")
+    
 
 def augment_data(data: np.ndarray, labels: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     """
@@ -38,26 +34,9 @@ def augment_data(data: np.ndarray, labels: np.ndarray) -> tuple[np.ndarray, np.n
     Returns:
         tuple[np.ndarray, np.ndarray]: 
     """
-    # raise NotImplementedError("Need to implement for task 2.2")
+    raise NotImplementedError("Need to implement for task 2.2")
 
-    transform = A.Compose([
-        A.HorizontalFlip(p=0.5),
-        A.RandomBrightnessContrast(p=0.1),
-        A.RandomCrop(width=64, height=64),
-    ])
 
-    aug_images = []
-    aug_labels = []
-    for im, label in zip(data, labels):
-        for _ in range(5):
-            transformed = transform(image=im)["image"]
-            aug_images.append(transformed)
-            aug_labels.append(label)
-
-    aug_images = np.array(aug_images)
-    aug_labels = np.array(aug_labels)
-
-    return aug_images, aug_labels
 
 def split_train_test_dataset(data: np.ndarray, labels: np.ndarray, percentage: float = 0.8, shuffle: bool = False) -> tuple[tuple[np.ndarray, np.ndarray], tuple[np.ndarray, np.ndarray]]:
     """
@@ -72,17 +51,8 @@ def split_train_test_dataset(data: np.ndarray, labels: np.ndarray, percentage: f
     Returns:
         tuple[tuple[np.ndarray, np.ndarray], tuple[np.ndarray, np.ndarray]]: 
     """
+    raise NotImplementedError("Need to implement for task 2.3")
 
-    if shuffle:
-        indices = np.random.permutation(len(data))
-        data = data[indices]
-        labels = labels[indices]
-
-    n = int(len(data) * percentage)
-    train_data, test_data = data[:n], data[n:]
-    train_labels, test_labels = labels[:n], labels[n:]
-
-    return (train_data, train_labels), (test_data, test_labels)
 
 
 def train_kNN(data: np.ndarray, labels: np.ndarray, k: int) -> np.ndarray:
@@ -97,11 +67,8 @@ def train_kNN(data: np.ndarray, labels: np.ndarray, k: int) -> np.ndarray:
     Returns: predictions
         np.ndarray 
     """
-    # raise NotImplementedError("Need to implement for task 2.3")
-    kNN = KNeighborsClassifier(n_neighbors=k)
-    data = data.reshape(data.shape[0], -1)
-    kNN.fit(data, labels)
-    return kNN
+    raise NotImplementedError("Need to implement for task 2.3")
+    
 
 def predict_kNN(kNN: KNeighborsClassifier, data: np.ndarray) -> np.ndarray:
     """
@@ -114,9 +81,8 @@ def predict_kNN(kNN: KNeighborsClassifier, data: np.ndarray) -> np.ndarray:
     Returns:
         np.ndarray: 
     """
-    # raise NotImplementedError("Need to implement for task 2.3")
-    data = data.reshape(data.shape[0], -1)
-    return kNN.predict(data)
+    raise NotImplementedError("Need to implement for task 2.3")
+    
 
 def evaluate_predictions(ground_truth: np.ndarray, labels: np.ndarray, metric: Callable) -> float:
     """
@@ -130,5 +96,5 @@ def evaluate_predictions(ground_truth: np.ndarray, labels: np.ndarray, metric: C
     Returns:
         float: 
     """
-    # raise NotImplementedError("Need to implement for task 2.3")
-    return metric(ground_truth, labels)
+    raise NotImplementedError("Need to implement for task 2.3")
+    
