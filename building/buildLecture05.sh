d2b97822b670da638fb3c37171d236fdf3964b72
Anders Kaestner <anders.kaestner@psi.ch>
2025-03-19 17:28:54 +0100

    Final before the lecture.

--- building/buildLecture05.sh
+++ building/buildLecture05.sh
@@ -1,3 +1,5 @@
 jupyter-book build Lectures/Lecture-05 --builder pdflatex
 
-cp Lectures/Lecture-05/_build/latex/QBI-Lecture05-AdvancedSegmentation.pdf docs/
+
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook \
+   -dNOPAUSE -dQUIET -dBATCH -sOutputFile=docs/QBI-Lecture05-AdvancedSegmentation.pdf Lectures/Lecture-05/_build/latex/QBI-Lecture05-AdvancedSegmentation.pdf 
