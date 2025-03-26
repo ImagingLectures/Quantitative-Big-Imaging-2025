8373f2144700d93de090d36a8b2c67d13319f96f
Anders Kaestner <anders.kaestner@psi.ch>
2025-03-26 16:48:11 +0100

    Evening before lecture 6 commit

--- building/buildLecture06.sh
+++ building/buildLecture06.sh
@@ -1,3 +1,4 @@
 jupyter-book build Lectures/Lecture-06 --builder pdflatex
 
-cp Lectures/Lecture-06/_build/latex/QBI-Lecture06-ShapeAnalysis.pdf docs/
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook \
+   -dNOPAUSE -dQUIET -dBATCH -sOutputFile=docs/QBI-Lecture06-ShapeAnalysis.pdf Lectures/Lecture-06/_build/latex/QBI-Lecture06-ShapeAnalysis.pdf 
