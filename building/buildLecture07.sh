d70d2db8f0b8ddc5bf5856b5a3cde913837d28af
Anders Kaestner <anders.kaestner@psi.ch>
2025-03-27 11:05:45 +0100

    Post lecture 6

--- building/buildLecture07.sh
+++ building/buildLecture07.sh
@@ -4,4 +4,5 @@ unzip Lectures/Lecture-07/data/Cropped_prediction_8bit.npy.zip
 
 jupyter-book build Lectures/Lecture-07 --builder pdflatex
 
-cp Lectures/Lecture-07/_build/latex/QBI-Lecture07-ComplexShape.pdf docs/
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook \
+   -dNOPAUSE -dQUIET -dBATCH -sOutputFile=docs/QBI-Lecture07-ComplexShape.pdf Lectures/Lecture-07/_build/latex/QBI-Lecture07-ComplexShape.pdf
