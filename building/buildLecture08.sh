f08dbb0e970dbce5c9f93af2ec64f38d0c1ac30c
Anders Kaestner <anders.kaestner@psi.ch>
2025-04-06 18:17:18 +0200

    Updated build script

--- building/buildLecture08.sh
+++ building/buildLecture08.sh
@@ -1,3 +1,5 @@
 jupyter-book build Lectures/Lecture-08 --builder pdflatex
 
-cp Lectures/Lecture-08/_build/latex/QBI-Lecture08-Statistics.pdf docs/
+# cp Lectures/Lecture-08/_build/latex/QBI-Lecture08-Statistics.pdf docs/
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook \
+   -dNOPAUSE -dQUIET -dBATCH -sOutputFile=docs/QBI-Lecture08-Statistics.pdf Lectures/Lecture-08/_build/latex/QBI-Lecture08-Statistics.pdf
