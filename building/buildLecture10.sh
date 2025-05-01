84da87bc1fe36b45ff67dfd85ed5629a990a76a8
Anders Kaestner <anders.kaestner@psi.ch>
2025-05-01 17:44:21 +0200

    Lecture 10 is essentially complete

--- building/buildLecture10.sh
+++ building/buildLecture10.sh
@@ -1,3 +1,6 @@
 jupyter-book build Lectures/Lecture-10 --builder pdflatex
 
-cp Lectures/Lecture-10/_build/latex/QBI-Lecture10-BimodalExperiments.pdf docs/
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook \
+   -dNOPAUSE -dQUIET -dBATCH -sOutputFile=docs/QBI-Lecture10-BimodalExperiments.pdf Lectures/Lecture-10/_build/latex/QBI-Lecture10-BimodalExperiments.pdf
+
+# cp Lectures/Lecture-10/_build/latex/QBI-Lecture10-BimodalExperiments.pdf docs/
