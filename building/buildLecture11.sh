c65516377cae2322ee77b120fbc5378405582738
Anders Kaestner <anders.kaestner@psi.ch>
2025-05-11 21:09:19 +0200

    Added some more note to the lecture script.

--- building/buildLecture11.sh
+++ building/buildLecture11.sh
@@ -1,3 +1,11 @@
-jupyter-book build Lectures/Lecture-11 --builder pdflatex
+#!/bin/bash
+# Usage: save as run-pytest.sh, make it executable
 
-cp Lectures/Lecture-11/_build/latex/QBI-Lecture11-ScalingUp.pdf docs/
+if command -v pixi &> /dev/null && pixi run which "jupyter-book" &> /dev/null; then
+    pixi run jupyter-book build Lectures/Lecture-11 --builder pdflatex
+else
+    jupyter-book build Lectures/Lecture-11 --builder pdflatex
+fi
+
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook \
+   -dNOPAUSE -dQUIET -dBATCH -sOutputFile=docs/QBI-Lecture11-ScalingUp.pdf Lectures/Lecture-11/_build/latex/QBI-Lecture11-ScalingUp.pdf
